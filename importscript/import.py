265be3833b734dac23d8d2a17ae02501ec058464
yanivomc-k1 <yanivomc@gmail.com>
2021-04-17 22:10:45 +0300

    added import script

---
+++ importscript/import.py
@@ -0,0 +1,29 @@
+import csv
+from collections import deque
+import elasticsearch
+from elasticsearch import helpers
+
+def readMovies():
+    csvfile= open('movies.csv', 'r')
+    reader = csv.DictReader( csvfile)
+    titleLookup= {}
+    for movie in reader:
+        titleLookup[movie['movieId']] = movie['title']
+        return titleLookup
+def readTags():
+    csvfile= open('ml-latest-small/tags.csv', 'r')
+    titleLookup= readMovies()
+    reader = csv.DictReader( csvfile)
+    for line in reader:
+        tag = {}
+        tag['user_id'] = int(line['userId'])
+        tag['movie_id'] = int(line['movieId'])
+        tag['title'] = titleLookup[line['movieId']]
+        tag['tag'] = line['tag']
+        tag['timestamp'] = int(line['timestamp'])
+        yield tag
+
+es= elasticsearch.Elasticsearch("34.222.6.59:9200")
+es.indices.delete(index="tags",ignore=404)
+deque(helpers.parallel_bulk(es,readTags(),index="tags"), maxlen=0)
+es.indices.refresh()
