73f6db33960a6aed5d23f3b5c54fb4919be75f84
yanivomc-k1 <yanivomc@gmail.com>
2021-04-17 23:57:01 +0300

    fixed import.py and added transformation script

--- importscript/import.py
+++ importscript/import.py
@@ -4,26 +4,36 @@ import elasticsearch
 from elasticsearch import helpers
 
 def readMovies():
-    csvfile= open('movies.csv', 'r')
-    reader = csv.DictReader( csvfile)
-    titleLookup= {}
-    for movie in reader:
-        titleLookup[movie['movieId']] = movie['title']
-        return titleLookup
-def readTags():
-    csvfile= open('tags.csv', 'r')
-    titleLookup= readMovies()
-    reader = csv.DictReader( csvfile)
-    for line in reader:
-        tag = {}
-        tag['user_id'] = int(line['userId'])
-        tag['movie_id'] = int(line['movieId'])
-        tag['title'] = titleLookup[line['movieId']]
-        tag['tag'] = line['tag']
-        tag['timestamp'] = int(line['timestamp'])
-        yield tag
+    csvfile = open('movies.csv', 'r')
 
-es= elasticsearch.Elasticsearch(['http://localhost:9200'], http_auth=('elastic', 'changeme'))
-es.indices.delete(index="tags",ignore=404)
-deque(helpers.parallel_bulk(es,readTags(),index="tags"), maxlen=0)
+    reader = csv.DictReader( csvfile )
+
+    titleLookup = {}
+
+    for movie in reader:
+            titleLookup[movie['movieId']] = movie['title']
+
+    return titleLookup
+
+def readRatings():
+    csvfile = open('ratings.csv', 'r')
+
+    titleLookup = readMovies()
+
+    reader = csv.DictReader( csvfile )
+    for line in reader:
+        rating = {}
+        rating['user_id'] = int(line['userId'])
+        rating['movie_id'] = int(line['movieId'])
+        rating['title'] = titleLookup[line['movieId']]
+        rating['rating'] = float(line['rating'])
+        rating['timestamp'] = int(line['timestamp'])
+        yield rating
+
+
+es = elasticsearch.Elasticsearch(['http://localhost:9200'], http_auth=('elastic', 'changeme'))
+
+es.indices.delete(index="ratings",ignore=404)
+deque(helpers.parallel_bulk(es,readRatings(),index="ratings"), maxlen=0)
 es.indices.refresh()
+
