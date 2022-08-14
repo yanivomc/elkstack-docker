310cd108dd0cfb57a190028c2cdc931316ed5617
root <admin@example.com>
2022-08-14 14:27:32 +0300

    updated

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -5,6 +5,7 @@ git clone https://github.com/yanivomc/elkstack-docker.git 2>&1 || true
 date >> /tmp/elkstack.logs
 pwd >> /tmp/elkstack.logs
 cd /tmp/work/elkstack-docker
+git pull >> /tmp/git.logs
 pwd >> /tmp/elkstack.logs
-docker-compose up -d >> /tmp/elkstack.logs
+docker-compose up --build >> /tmp/docker.logs
 docker-compose logs -f >> /tmp/elkstack.logs
