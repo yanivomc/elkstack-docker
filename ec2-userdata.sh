676fb9e2c532185faf78a606a416b0eb0f244801
root <admin@example.com>
2022-08-14 14:17:30 +0300

    update folder

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -4,7 +4,7 @@ cd /tmp/work/
 git clone https://github.com/yanivomc/elkstack-docker.git 2>&1 || true
 date >> /tmp/elkstack.logs
 pwd >> /tmp/elkstack.logs
-cd /tmp/elkstack-docker
+cd /tmp/work/elkstack-docker
 pwd >> /tmp/elkstack.logs
 docker-compose up -d >> /tmp/elkstack.logs
 docker-compose logs -f >> /tmp/elkstack.logs
